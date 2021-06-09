11a3f28a7ecb26f5b4267029e8e0b24e0c75650b
Shakena <shakenacme@gmail.com>
2021-06-09 03:32:41 -0400

    Header: Added JS to open and close hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const btnhamburger = document.querySelector('#btnHamburger').addEventListener('click', function() {
+   console.log('open Hamburger');
+   // if btnHamburger contains class 'open'
+   if(btnHamburger.classList.contains('open')) {
+      // remove 'open'
+      btnHamburger.classList.remove('open');
+   }
+   else {
+      // add 'open'
+      btnHamburger.classList.add('open');
+   }
+});
