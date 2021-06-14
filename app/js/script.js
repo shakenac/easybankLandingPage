d5ea4fdb8ddbb8e2eaaedf83e83532e56d02326e
Shakena <shakenacme@gmail.com>
2021-06-14 17:45:39 -0400

    Header: Created and added fade-out to overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,15 +1,21 @@
 const btnhamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
 
 btnHamburger.addEventListener('click', function() {
-
+   // close hamburger menu
    // if btnHamburger contains class 'open'
    if(header.classList.contains('open')) {
       // remove 'open'
       header.classList.remove('open');
+      overlay.classList.remove('fade-in');
+      overlay.classList.add('fade-out');
    }
+   // Open hamburger menu
    else {
       // add 'open'
       header.classList.add('open');
+      overlay.classList.remove('fade-out');
+      overlay.classList.add('fade-in');
    }
 });
