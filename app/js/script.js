5baa751ded6783bea27f0ba70698bdbaa5d660ae
Shakena <shakenacme@gmail.com>
2021-06-14 23:10:32 -0400

    Body: Added a noscroll class to prevent scrolling when mobile menu is open

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@ const btnhamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
 const fadeElems = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function() {
    // close hamburger menu
@@ -9,6 +10,7 @@ btnHamburger.addEventListener('click', function() {
    if(header.classList.contains('open')) {
       // remove 'open'
       header.classList.remove('open');
+      body.classList.remove('noscroll');
       fadeElems.forEach(function(element) {
          element.classList.remove('fade-in');
          element.classList.add('fade-out');
@@ -19,6 +21,7 @@ btnHamburger.addEventListener('click', function() {
    else {
       // add 'open'
       header.classList.add('open');
+      body.classList.add('noscroll');
       fadeElems.forEach(function(element) {
          element.classList.remove('fade-out');
          element.classList.add('fade-in');
